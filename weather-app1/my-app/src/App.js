3c33b4eb1abc832f6204294b69486ddc40fbdf0a
Елена Пальчикова <Parentsclub56@gmail.com>
2023-01-03 19:49:13 +0100

    2

--- weather-app1/my-app/src/App.js
+++ weather-app1/my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, Vasilisa is online!)
+          Hello, Vasilisa is online!:)
         </p>
         <a
           className="App-link"
